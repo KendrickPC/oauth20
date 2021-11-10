53cc717e19c49b98b0017fc7a31fce347a14a96c
kendrickPC <kennethpchang@gmail.com>
2021-11-10 11:04:30 -0800

    User properly stored in MongoDB database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys')
+const User = require('../models/user-model')
 
 passport.use(
   new GoogleStrategy({
@@ -9,9 +10,21 @@ passport.use(
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
-    // passport callback function
-    console.log('passport callback function fired');
-    console.log(profile)
+    // check if user already exists in our database
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+      if (currentUser) {
+        // already have a user
+        console.log('User is: ' + currentUser)
+      } else {
+        // create a user in our database
+        new User({
+          username: profile.displayName,
+          googleId: profile.id,
+        }).save().then((newUser) => {
+          console.log('New User Created: ' + newUser)
+        })
+      }
+    })
   })
 )
 
