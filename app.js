56c315e94a7aa3457275d97e1c358cc834172a56
kendrickPC <kennethpchang@gmail.com>
2021-11-10 12:43:26 -0800

    Redirected users who are not logged in

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes')
+const profileRoutes = require('./routes/profile-routes')
 const passportSetup = require('./config/passport-setup')
 
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
-
 // set up a view engine
 app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}))
+
+// initialize passport
+app.use(passport.initialize())
+app.use(passport.session());
 
 // connect to MongoDB
 mongoose.connect(keys.mongodb.dbURI, () => {
@@ -18,6 +29,7 @@ mongoose.connect(keys.mongodb.dbURI, () => {
 
 // set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 // create home route
 app.get('/', (req, res) => {
